fedf1da892bd50a1002344620c3f2041db7ef79b
J Scott Rumptz <jscott@rumptz.tech>
2021-04-19 15:03:17 -0500

    added global state

---
+++ client/src/components/CategoryMenu/index.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_CATEGORIES } from "../../utils/queries";
+
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+
+function CategoryMenu() {
+  // const categories = categoryData?.categories || [];
+
+  // we immediately call upon the useStoreContext() Hook to retrieve the current state from the global
+  // state object and the dispatch() method to update state.
+  const [state, dispatch] = useStoreContext();
+  // Because we only need the categories array out of our global state, we simply destructure it out of
+  // state so we can use it to provide to our returning JSX.
+  const { categories } = state;
+
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  // when this component loads and the response from the useQuery() Hook returns, the useEffect() Hook
+  // notices that categoryData is not undefined anymore and runs the dispatch() function, setting our
+  // category data to the global state!
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
+
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map(item => (
+        <button
+          key={item._id}
+          onClick={() => {
+            handleClick(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryMenu;
---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,42 @@
+// createContext will be used to instantiate a new Context object. The more meaningful term we can use
+// here is that we're using it to create the container to hold our global state data and functionality
+// so we can provide it throughout our app!
+//
+// useContext is another React Hook that will allow us to use the state created from the createContext
+// function.
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+
+// When we run the createContext() function, it creates a new Context object. 
+const StoreContext = createContext();
+// Every Context object comes with two components, a Provider and Consumer. The Provider is a special
+// type of React component that we wrap our application in so it can make the state data that's passed
+// into it as a prop available to all other components. The Consumer is our means of grabbing and
+//  using the data that the Provider holds for us.
+const { Provider } = StoreContext;
+
+// With this function, StoreProvider, we instantiate our initial global state with the
+// useProductReducer() function we created earlier. Because that wraps it around the useReducer()
+// Hook from React, every time we run this useProductReducer() function, we receive the following
+// two items in return:
+const StoreProvider = ({ value = [], ...props }) => {
+    // state is the most up-to-date version of our global state object.
+    //
+    // dispatch is the method we execute to update our state. It is specifically going to look for
+    // an action object passed in as its argument.
+    const [state, dispatch] = useProductReducer({
+      products: [],
+      categories: [],
+      currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+  };
+
+  const useStoreContext = () => {
+    return useContext(StoreContext);
+  };
+
+  export { StoreProvider, useStoreContext };
